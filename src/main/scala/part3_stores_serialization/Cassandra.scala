e718c1f9cda7fcf743d51abdf2d891393e36e05d
Sergio Cano <serdeliverance@gmail.com>
2020-06-15 11:54:29 -0300

    cassandra local store configuration

--- src/main/scala/part3_stores_serialization/Cassandra.scala
+++ src/main/scala/part3_stores_serialization/Cassandra.scala
@@ -1,5 +1,20 @@
 package part3_stores_serialization
 
-object Cassandra {
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
 
+object Cassandra extends App {
+
+  val cassandraActorSystem = ActorSystem("cassandraActorSystem", ConfigFactory.load().getConfig("cassandraDemo"))
+  val persistentActor = cassandraActorSystem.actorOf(Props[SimplePersistentActor], "simplePersistentActor")
+
+  for (i <- 1 to 10) {
+    persistentActor ! s"I love Akka [$i]"
+  }
+  persistentActor ! "print"
+  persistentActor ! "snap"
+
+  for (i <- 11 to 20) {
+    persistentActor ! s"I love Akka [$i]"
+  }
 }
--- src/main/scala/part3_stores_serialization/SimplePersistentActor.scala
+++ src/main/scala/part3_stores_serialization/SimplePersistentActor.scala
@@ -1,5 +1,33 @@
 package part3_stores_serialization
 
-class SimplePersistentActor {
+import akka.actor.ActorLogging
+import akka.persistence.{PersistentActor, RecoveryCompleted, SaveSnapshotFailure, SaveSnapshotSuccess, SnapshotOffer}
 
+class SimplePersistentActor extends PersistentActor with ActorLogging {
+
+  // mutable state
+  var nMessages = 0
+
+  override def persistenceId: String = "single-persistent-actor"
+
+  override def receiveCommand: Receive = {
+    case "print" => log.info(s"I have persisted: $nMessages so far")
+    case "snap" => saveSnapshot(nMessages)
+    case message => persist(message) { _ =>
+      log.info(s"Persisting $message")
+      nMessages += 1
+    }
+    case SaveSnapshotSuccess(metadata) => log.info(s"Save snapshot was sucessful: $metadata")
+    case SaveSnapshotFailure(_, cause) => log.warning(s"Save snapshot failed: $cause")
+  }
+
+  override def receiveRecover: Receive = {
+    case RecoveryCompleted => log.info("Recovery done")
+    case SnapshotOffer(_, payload: Int) =>
+      log.info(s"Recovered snapshot: $payload")
+      nMessages = payload
+    case message =>
+      log.info(s"Recovered: $message")
+      nMessages += 1
+  }
 }
